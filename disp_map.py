78e7bb90fa5b9f4b53e79d8c64d37fd6c65fbe45
Majeed <majeedk526@gmail.com>
2017-03-17 00:40:53 -0800

    modified disparity map calculation to use lcok for synchronisation

--- disp_map.py
+++ disp_map.py
@@ -1,9 +1,19 @@
 import numpy as np
 import cv2
 from matplotlib import pyplot as plt
-imgL = cv2.imread('im0.png',0)
-imgR = cv2.imread('im1.png',0)
-stereo = cv2.StereoBM_create(numDisparities=16, blockSize=15)
-disparity = stereo.compute(imgL,imgR)
-plt.imshow(disparity,'gray')
-plt.show()
+
+def calc_disp_map(lock, stop)
+
+    while(not stop)
+        lock.acquire()
+        try:    
+            imgL = cv2.imread('imgL.jpg',0)
+            imgR = cv2.imread('imgR.jpg',0)
+            stereo = cv2.StereoBM_create(numDisparities=16, blockSize=15)
+            disparity = stereo.compute(imgL,imgR) # calculated disparity
+            #plt.imshow(disparity,'gray')
+            #plt.show()
+        finally:
+            lock.release()
+
+    print('exiting disp calculations')
