0161ab463b8a554adeacfafddf524185f72a64d3
Majeed <majeedk526@gmail.com>
2017-04-14 11:04:54 -0800

    fix: camera_controller

--- camera_controller.py
+++ camera_controller.py
@@ -1,12 +1,9 @@
-#import cv2
-import picamera
+import cv2
 import time
-#from time import time, sleep
-
-camera = picamera.PiCamera()
 
 def start_capture(lock, stop):
     "captures images from cameras"
+    config()
     count=0
     while(not stop):
         lock.acquire()
@@ -16,12 +13,36 @@ def start_capture(lock, stop):
             count += 1
         print(('%s %d'), ('lock acquired by camera : ', count))
         try:
-            camera.capture('imgL.jpg')
-            camera.capture('imgR.jpg')
+            ret1, frame1 = cam1.read()
+            ret2, frame2 = cam2.read()
+            cv2.imwrite('imgL.jpg', frame1)
+            cv2.imwrite('imgR.jpg', frame2)
         finally:
             print('lock released by camera')    
             lock.release();
             time.sleep(1)
 
+    release()
     print('stopping image capture')
     
+def config():
+    print('configureing camera')
+    global cam1, cam2
+    cam1 = cv2.VideoCapture(0)
+    cam2 = cv2.VideoCapture(1)
+    cam1.set(cv2.CAP_PROP_FPS,10)
+    cam2.set(cv2.CAP_PROP_FPS,10)    
+
+def release():
+    print('releasing camera resource')
+    cam1.release()
+    cam2.release()
+    cv2.destroyAllWindows()
+    
+def single_capture():
+    config()
+    ret1, frame1 = cam1.read()
+    ret2, frame2 = cam2.read()
+    cv2.imwrite('imgL.jpg', frame1)
+    cv2.imwrite('imgR.jpg', frame2)
+    release()
