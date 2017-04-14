01213958eb146aa58eef3f11eafeecef2ff190e4
Majeed <majeedk526@gmail.com>
2017-04-14 11:22:47 -0800

    fix: disp_map

--- disp_map.py
+++ disp_map.py
@@ -15,7 +15,7 @@ def calc_disp_map(lock, stop):
         try:    
             imgL = cv2.imread('imgL.jpg',0)
             imgR = cv2.imread('imgR.jpg',0)
-            stereo = cv2.StereoBM_create(numDisparities=16, blockSize=15)
+            stereo = cv2.StereoBM_create(numDisparities=16, blockSize=61)
             disparity = stereo.compute(imgL,imgR) # calculated disparity
             plt.imshow(disparity,'gray')
             #plt.show()
@@ -25,3 +25,11 @@ def calc_disp_map(lock, stop):
             time.sleep(1)
 
     print('exiting disp calculations')
+
+def single_disp():
+    imgL = cv2.imread('imgL.jpg',cv2.IMREAD_GRAYSCALE)
+    imgR = cv2.imread('imgR.jpg',cv2.IMREAD_GRAYSCALE)
+    stereo = cv2.StereoBM_create(numDisparities=16, blockSize=61)
+    disparity = stereo.compute(imgL,imgR) # calculated disparity
+    plt.imshow(disparity,'gray')
+    plt.show() 
